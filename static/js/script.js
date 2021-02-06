64e43f89e69ca4bcf0011ab70549e53201f32e82
opera7133 <ainznino@pm.me>
2021-02-07 00:10:39 +0900

    anim

--- static/js/script.js
+++ static/js/script.js
@@ -1,24 +1,3 @@
-var mySwiper = new Swiper(".swiper-container", {
-  // Optional parameters
-  direction: "horizontal",
-  loop: true,
-  speed: 550,
-  autoplay: {
-    delay: 6000,
-  },
-
-  // If we need pagination
-  pagination: {
-    el: ".swiper-pagination",
-  },
-
-  // Navigation arrows
-  navigation: {
-    nextEl: ".swiper-button-next",
-    prevEl: ".swiper-button-prev",
-  },
-});
-
 function toggleNav() {
   var hamburger = document.getElementById("hamburgerbtn");
   var overlay = document.getElementById("overlay");
