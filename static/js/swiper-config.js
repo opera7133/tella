d197639e60bf15274ee725c98d02616f49618654
matthh9797 <matthewh@netextremity.com>
2023-03-27 16:02:28 +0700

    add swiper

--- static/js/swiper-config.js
+++ static/js/swiper-config.js
@@ -13,3 +13,21 @@ var mySwiper = new Swiper(".swiper-container", {
         prevEl: ".swiper-button-prev",
     },
 });
+var swiper = new Swiper(".mySwiper", {
+    loop: true,
+    spaceBetween: 10,
+    slidesPerView: 4,
+    freeMode: true,
+    watchSlidesProgress: true,
+});
+var swiper2 = new Swiper(".mySwiper2", {
+    loop: true,
+    spaceBetween: 10,
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+    },
+    thumbs: {
+        swiper: swiper,
+    },
+});
